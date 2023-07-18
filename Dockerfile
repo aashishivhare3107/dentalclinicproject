52ff65cff9a0ac803c386ed8d2676b4f30637b4c
aashishivhare3107 <aashi.shivhare_cs19@gla.ac.in>
2023-07-18 22:06:50 +0530

    final commit

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,17 @@
-FROM node:18.16.1
+FROM node:17-alpine
+
+# We use nodemon to restart the server every time there's a change
+RUN npm install -g nodemon
+
 WORKDIR /app
-COPY package*.json ./
+
+COPY package.json .
+
 RUN npm install
+
 COPY . .
-EXPOSE 3000 5000
-CMD [ "npm", "start", "3000", "5000" ]
+
+EXPOSE 5000
+
+# Use script specified in package,json
+CMD ["npm", "run", "dev"]
--- server.js
+++ server.js
@@ -6,13 +6,18 @@ const userRouter = require("./routes/userRoutes");
 const doctorRouter = require("./routes/doctorRoutes");
 const appointRouter = require("./routes/appointRoutes");
 const path = require("path");
-const notificationRouter = require("./routes/notificationRouter");
-
+const notificationRouter = require("./routes/notificationRouter");  
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  console.log(__dirname)
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+ });
+ app.use(express.static("public"));
+
 app.use("/api/user", userRouter);
 app.use("/api/doctor", doctorRouter);
 app.use("/api/appointment", appointRouter);
